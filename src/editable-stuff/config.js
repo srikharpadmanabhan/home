a39307403a8001229146c05b661c31bee7c102b6
srikharpadmanabhan <65561220+srikharpadmanabhan@users.noreply.github.com>
2021-07-26 18:41:13 -0400

    added new repo for marvel api project

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -64,7 +64,7 @@ const repos = {
   heading: "Recent Projects",
   gitHubUsername: "srikharpadmanabhan", //i.e."johnDoe12Gh"
   reposLength: 0,
-  specificRepos: ["personal-portfolio-site", "react-website-first", "kaggle-duplicate-quora", "todo-list-and-recipe-application"],
+  specificRepos: ["personal-portfolio-site", "react-website-first", "kaggle-duplicate-quora", "todo-list-and-recipe-application", "marvel-api-displayer"],
 };
 
 // Leadership SECTION
