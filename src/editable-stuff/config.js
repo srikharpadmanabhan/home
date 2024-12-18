a0defa553adacc727003134468fc25eff3f3a291
srikhar-padmanabhan <srikharp@umich.edu>
2024-12-18 17:03:12 -0500

    updated username and repos for git

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -62,7 +62,7 @@ const about = {
 const repos = {
   show: true,
   heading: "Recent Projects",
-  gitHubUsername: "srikharpadmanabhan", //i.e."johnDoe12Gh"
+  gitHubUsername: "srikhar-padmanabhan", //i.e."johnDoe12Gh"
   reposLength: 0,
   specificRepos: ["mm-data-analysis-final-project", "personal-portfolio-site", "react-website-first", "kaggle-duplicate-quora", "todo-list-and-recipe-application", "marvel-api-displayer"],
 };
